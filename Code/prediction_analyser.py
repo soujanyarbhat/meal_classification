8ede40d6c331d3fa5890dd5beda866d6009f4048
Varun Chaudhary <varchd250295@gmail.com>
2019-11-10 12:28:09 -0700

    added prediction analyser to check accuracy of test script output

---
+++ Code/prediction_analyser.py
@@ -0,0 +1,27 @@
+# script that checks the acuuracy of test set(mimic-ed as unseen records)
+
+import os
+from os import listdir
+from os.path import isfile, join
+
+import pandas as pd
+
+current_dir = os.path.dirname(__file__)
+input_path = os.path.join(current_dir, '..', 'Output')
+truth_path = os.path.join(current_dir, '..', 'Test', 'GroundTruth.csv')
+# prediction files for all members
+input_files = [f for f in listdir(input_path) if isfile(join(input_path, f))]
+# ground truth DF
+ground_truth_df = pd.read_csv(truth_path, header = None)
+print('GROUND TRUTH ------------------------------\n', ground_truth_df)
+# Accuracy
+for input_file in input_files:
+    predictions_df = pd.read_csv(os.path.join(input_path, input_file), header = None)
+    accuracy_df = ground_truth_df.where(ground_truth_df.values == predictions_df.values).notna()
+    print('PREDICTIONS ------------------------------\n', predictions_df)
+    print('ACCURACY ------------------------------\n', accuracy_df)
+    hits = accuracy_df.sum()
+    rows, cols = ground_truth_df.shape
+    misses = rows - hits
+    accuracy = (hits / rows) * 100
+    print('{} -> {}'.format(input_file, accuracy))
