6b30db921f573b9ae07c485e735dfd2e40d1e0c4
Varun Chaudhary <varchd250295@gmail.com>
2019-12-11 11:20:18 -0700

    Test script- now takes file path(not dir)

--- Phase3/Code/testing_mealClustering.py
+++ Phase3/Code/testing_mealClustering.py
@@ -15,7 +15,7 @@ class TestingMealClustering():
     OUTPUT_PATH_MODEL = os.path.join(os.path.dirname(__file__), '..', 'Model')
 
     def __init__(self):
-        print("Meal Clustering Model ...")
+        print("Testing Meal Clustering Model ...")
 
 
     def read_path(self):
@@ -23,17 +23,11 @@ class TestingMealClustering():
         Input path for data set- USER INPUT
         :return: input folder path
         """
-        print("Reading input directory ... ")
+        print("Reading input file path ... ")
 
-        current_dir = os.path.dirname(__file__)
-        folder_path = os.path.join(current_dir, '..', 'Input')
-        pre_configured_path = os.path.abspath(folder_path)
-        temp_data_set_folder = input('Please enter the data set directory path\n'
-                                     'OR\nHit enter to use %s: ' % pre_configured_path)
-        if temp_data_set_folder.strip() != '':
-            folder_path = temp_data_set_folder.strip()
+        folder_path = input('Please enter the file path </path/to/meal.csv> : \n')
 
-        print("Reading input directory ... DONE.")
+        print("Reading input file path ... DONE.")
 
         return folder_path
 
@@ -49,12 +43,7 @@ class TestingMealClustering():
         col_names = ["Col1", "Col2", "Col3", "Col4", "Col5", "Col6", "Col7", "Col8", "Col9", "Col10",
                      "Col11", "Col12", "Col13", "Col14", "Col15", "Col16", "Col17", "Col18", "Col19", "Col20",
                      "Col21", "Col22", "Col23", "Col24", "Col25", "Col26", "Col27", "Col28", "Col29", "Col30", "Col31"]
-        # Lists all files in input directory
-        input_files = [f for f in listdir(input_path) if isfile(join(input_path, f))]
-        raw_meal_df = pd.DataFrame()
-        for input_file in input_files:
-            input_df = pd.read_csv(os.path.join(input_path, input_file), names=col_names)
-            raw_meal_df = pd.concat([raw_meal_df, input_df])
+        raw_meal_df = pd.read_csv(os.path.join(input_path, input_path), names=col_names)
 
         print("Meal data size - ", raw_meal_df.shape)
 
